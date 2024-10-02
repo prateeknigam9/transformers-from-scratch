b1bd0647e8ef85ff6f30bcb620e1ec53f72e8022
prateeknigam <prateeknigam9@gmail.com>
2024-10-02 13:07:48 +0100

    decoder

--- model.py
+++ model.py
@@ -150,12 +150,49 @@ class EncoderBlock(nn.Module):
 class Encoder(nn.Module):
     def __init__(self, layers: nn.ModuleList) -> None:
         super().__init__()
-        self.layers = layers
+        self.layers = layers # n layers
         self.norm = LayerNormalization()
     
     def forward(self,x, mask):
         for layer in self.layers:
             x = layer(x,mask)
         return self.norm(x)
-            
-        
+
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block:MultiHeadAttentionBlock,
+                 cross_head_attention_block:MultiHeadAttentionBlock,
+                 feed_forward_block: FeedForwardBlock,
+                 dropout:float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_head_attention_block = cross_head_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+    
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connection[0](x, self.self_attention_block(x,x,x,tgt_mask))
+        x = self.residual_connection[1](x, self.cross_head_attention_block(x,encoder_output,encoder_output,src_mask))
+        x = self.residual_connection[2](x, self.feed_forward_block)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, layers:nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+    
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
+
+class ProjectionLayer(nn.Module):
+    def __init__(self, d_model:int, vocab_size:int) -> None:
+        super().__init__()
+        self.projection_layer = nn.Linear(d_model,vocab_size)
+    
+    def forward(self, x):
+        # (Batch, seq_len, d_model) --> (batch, seq_len, vocab_size)
+        return torch.log_softmax(self.projection_layer(x), dim=-1)
+
+    
