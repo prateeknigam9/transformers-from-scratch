82a45923025c7e26246f8e5b4f2e75c883a01413
prateeknigam <prateeknigam9@gmail.com>
2024-10-24 23:11:48 +0100

    updated code to publication

--- model_scripts/encoder_model.py
+++ model_scripts/encoder_model.py
@@ -1,6 +1,45 @@
 
 import torch.nn as nn
-from model_scripts import common_blocks
+import torch
+from model_scripts import new_common_blocks
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_ff: int, embedding_dim:int, dropout:float):
+        super().__init__()
+        self.l1 = nn.Linear(embedding_dim,d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.l2 = nn.Linear(d_ff, embedding_dim)
+        
+    def forward(self, x):
+        x = torch.relu(self.l1(x))
+        x = self.dropout(x)
+        x = self.l2(x)
+        return x
+        
+class encoder_block(nn.Module):
+    def __init__(self, self_attention_layer : new_common_blocks.MultiHeadAttention,
+                 feed_forward_layer: FeedForwardBlock,
+                 dropout:float):
+        super().__init__()
+        self.self_attention_layer = self_attention_layer
+        self.feed_forward_layer = feed_forward_layer
+        self.residual_connections = nn.ModuleList([new_common_blocks.ResidualConnections(dropout) for _ in range(2)])
+        self.norm = new_common_blocks.LayerNormalization()
+    
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_layer(x,x,x,src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_layer)
+        return self.norm(x)
+
+class Encoder(nn.Module):
+    def __init__(self, layers:nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x,mask)
+        return x        
 
 
 class ClassificationHead(nn.Module):
@@ -12,8 +51,8 @@ class ClassificationHead(nn.Module):
         
 class EncoderOnlyTransformer(nn.Module):
     def __init__(self,
-                 src_embed: common_blocks.InputEmbeddings, pos: common_blocks.PositionalEncoding,
-                 encoder:common_blocks.Encoder, classifier_layer :ClassificationHead):
+                 src_embed: new_common_blocks.InputEmbeddings, pos: new_common_blocks.PositionalEncoding,
+                 encoder:Encoder, classifier_layer :ClassificationHead):
         super().__init__()
         self.src_embed = src_embed
         self.pos = pos
@@ -37,17 +76,17 @@ def build_encoder_only_transformer(src_vocab_size, n_classes,
     dropout = config['model']['dropout']
     d_ff = config['model']['d_ff']
     
-    src_embed = common_blocks.InputEmbeddings(src_vocab_size,embedding_dim)
-    src_pos = common_blocks.PositionalEncoding(embedding_dim, seq_len, dropout)
+    src_embed = new_common_blocks.InputEmbeddings(src_vocab_size,embedding_dim)
+    src_pos = new_common_blocks.PositionalEncoding(embedding_dim, seq_len, dropout)
 
     encoder_blocks = []
     for _ in range(N):
-        self_attention_block = common_blocks.MultiHeadAttention(seq_len, embedding_dim, h, dropout)
-        feed_forward_block = common_blocks.FeedForwardBlock(d_ff,embedding_dim, dropout)
-        enc = common_blocks.encoder_block(self_attention_block,feed_forward_block,dropout)
+        self_attention_block = new_common_blocks.MultiHeadAttention(seq_len, embedding_dim, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_ff,embedding_dim, dropout)
+        enc = encoder_block(self_attention_block,feed_forward_block,dropout)
         encoder_blocks.append(enc)
 
-    encoder = common_blocks.Encoder(nn.ModuleList(encoder_blocks))
+    encoder = Encoder(nn.ModuleList(encoder_blocks))
     classifier = ClassificationHead(embedding_dim,n_classes)
     transformer = EncoderOnlyTransformer(src_embed, src_pos, encoder, classifier)
 
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Transformers from Scratch
+
+Hi!
+This repository contains an implementation of transformer models built from scratch, divided into three main components: 
+1. **Encoder-Only Model** 
+2. **Decoder-Only Model** 
+3. **Combined Encoder-Decoder Model**
+
+Transformers have revolutionized natural language processing and are foundational to many state-of-the-art models. This project aims to provide a clear and modular implementation of transformers, inspired by the work of 
+[Sebastian Raschka.](https://sebastianraschka.com/)
+
+## Motivation
+
+I started this project to better understand how transformers work. I wanted to learn the details of each step involved in building a transformer model. By coding everything from scratch, I could see how the different parts fit together and how they function.
+
+My goal was to become more skilled at working with transformers so that I could apply this knowledge to real-world tasks and research. By understanding the inner workings of transformers, I can also explore ways to improve or customize them for specific needs. Overall, this project helped me gain a deeper appreciation for this important technology in natural language processing.
+
+## Features - 
+
+**Modular Code**: Each component of the transformer is implemented in a modular format for easy understanding and extensibility. 
+**Customizable Architecture**: The models can be easily modified to suit specific tasks or research interests.
+
+## Directory Structure
+
+/transformers-from-scratch
+│
+├── encoder_only/
+│   ├── model.py
+│   ├── train.py
+│   └── evaluate.py
+│
+├── decoder_only/
+│   ├── model.py
+│   ├── train.py
+│   └── evaluate.py
+│
+└── combined_model/
+    ├── model.py
+    ├── train.py
+    └── evaluate.py
+
+
+
+Thankyou!
+[Prateek Nigam](https://prateeknigam9.github.io/PrateekNigam)
